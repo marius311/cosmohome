e7d05765948d8c1948f9221878b158e6abce91d7
marius <mmillea@ucdavis.edu>
2016-04-09 21:18:42 +0200

    update jobplot to cache results to sqlite db

--- apps/lsplitsims/bin/lsplitsims_jobplot.py
+++ apps/lsplitsims/bin/lsplitsims_jobplot.py
@@ -4,36 +4,44 @@ mpl.use('Agg')
 from pylab import *
 import MySQLdb as mdb
 import os.path as osp
+import sqlite3
+import re
 
-
-def getjobs(i): 
-    cur.execute(
-"""select w.name from workunit w 
-inner join result r on r.workunitid=w.id 
-where w.appid=5 and r.outcome%s"""%i)
-    return set([tuple(map(int,x[0].split('_')[-3:])) for x in cur.fetchall()])
-
-def maskmat(jobs):
-    return array([[(1. if (lslice[0],lslice[1],seed) in jobs else 0) for lslice in lslices] for seed in range(10000)]).T
-
-
+#get all current lsplitsims results
 con = mdb.connect('mysql', 'root', '', 'cosmohome')
 cur=con.cursor()
+cur.execute("""
+select id, hostid, userid, teamid, name, mod_time, validate_state, outcome from result
+where appid=5 and server_state=5;
+""")
+results=cur.fetchall()
+con.close()
 
-success_jobs, inprogress_jobs, error_jobs = (getjobs(i) for i in ['=1','=0','>1'])
+#add any new ones the sqlite db
+con = sqlite3.connect('../archives/lsplitsims.db')
+con.execute("""
+create table if not exists result
+(id INT PRIMARY KEY, hostid INT, userid INT, teamid INT, name TEXT, mod_time INT, validate_state INT, outcome INT)
+""")
+con.executemany("""insert or ignore into result values (%s)"""%','.join('?'*8),results)
+con.commit()
+names = [n for n, in con.execute('select name from result')]
+con.close()
 
-lslices=[]        
-for lsplit in range(100,2500,50)+[2509]:
-    if lsplit<1700: lslices.append((lsplit,2509))
-    for lmin in (2,30):
-        if lsplit>=650: lslices.append((lmin,lsplit))
+#scan job names to get seeds/lslices of completed jobs
+rc=re.compile('planck_param_sims_([0-9]+)_([0-9]+)_([0-9]+)_.*')
+dat=[map(int,rc.match(n).groups()) for n in names]
+jobs={}
+for d in dat:
+    j=jobs[d[2]]=jobs.get(d[2],set())
+    j.add(tuple(d[:2]))
 
-
+#make plot
 xkcd()
-matshow([maskmat(success_jobs).sum(axis=0)],aspect=1000,cmap='Blues')
-
+matshow([sorted([len(v) for k,v in jobs.items() if k>20000])[::-1]],aspect=1000,cmap='Blues')
+xlim(0,10000)
 gca().set_yticks([])
 gca().xaxis.set_ticks_position('bottom')
 xlabel('Number of Simulations Completed',size=16)
 gcf().set_size_inches(15,15)
-savefig(osp.join(osp.dirname(__file__),'../html/user/img/lsplitsims_jobplot.png'),bbox_inches='tight',dpi=74)
+savefig(osp.join(osp.dirname(__file__),'../html/user/img/lsplitsims_jobplot_new.png'),bbox_inches='tight',dpi=74)
