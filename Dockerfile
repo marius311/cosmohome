c5da248b013f376cef068433200771836b8f1274
marius <mmillea@ucdavis.edu>
2018-05-29 19:50:13 +0200

    upgrade to server_release/0.9 (including bootstrap)

--- Dockerfile
+++ Dockerfile
@@ -1,38 +1,41 @@
-FROM boinc/server_makeproject:1.4.2-b2d
+FROM boinc/server_makeproject:2.1.0-b2d
 
 MAINTAINER Marius Millea <mariusmillea@gmail.com>
 
 #dont need built-in boinc2docker app
-RUN rm -rf $PROJHOME/apps/boinc2docker
+RUN rm -rf $PROJECT_ROOT/apps/boinc2docker
 
 #install extra packages
-RUN apt-get update && apt-get install -y ruby-kramdown
+RUN apt-get update && apt-get install -y --no-install-recommends ruby-kramdown
 
 # install camb_legacy
-COPY apps/camb_legacy/ $PROJHOME
+COPY apps/camb_legacy/ $PROJECT_ROOT
 
 # install boinc2docker_camb
-COPY apps/camb_boinc2docker/ $PROJHOME
-RUN boinc2docker_create_app $PROJHOME/apps_boinc2docker/camb/boinc2docker.yml
+COPY apps/camb_boinc2docker/ $PROJECT_ROOT
+RUN boinc2docker_create_app --projhome $PROJECT_ROOT $PROJECT_ROOT/apps_boinc2docker/camb/boinc2docker.yml
 
 # install lsplitsims
-COPY apps/lsplitsims/ $PROJHOME
-RUN boinc2docker_create_app $PROJHOME/apps_boinc2docker/lsplitsims/boinc2docker.yml
+COPY apps/lsplitsims/ $PROJECT_ROOT
+RUN boinc2docker_create_app --projhome $PROJECT_ROOT $PROJECT_ROOT/apps_boinc2docker/lsplitsims/boinc2docker.yml
 
 # project files
-COPY py $PROJHOME/py
-COPY project.xml config.xml boincserver.httpd.conf $PROJHOME/
-COPY html $PROJHOME/html
-COPY bin $PROJHOME/bin
+COPY py $PROJECT_ROOT/py
+COPY project.xml config.xml cosmohome.httpd.conf $PROJECT_ROOT/
+COPY boinc/html $PROJECT_ROOT/html
+COPY html $PROJECT_ROOT/html
+COPY bootswatch/cyborg/bootstrap.min.css $PROJECT_ROOT/html/user
+COPY bin $PROJECT_ROOT/bin
+
 
 # private files and signing
-COPY private/ $PROJHOME/
+COPY private/ $PROJECT_ROOT/
 RUN bin/sign_all_apps \
     && bin/xml_merge --cleanup config.private.yml config.xml
 
 # compile markdown files
-RUN cd $PROJHOME/html/user && ./compile_md.py
+RUN cd $PROJECT_ROOT/html/user && ./compile_md.py
 
 # finish up
 ARG GITTAG
-RUN echo $GITTAG > $PROJHOME/.gittag
+RUN echo $GITTAG > $PROJECT_ROOT/.gittag
--- html/user/index.php
+++ html/user/index.php
@@ -33,38 +33,54 @@ if (defined("CHARSET")) {
 page_head(PROJECT, null, null, "yes");
 
 echo <<<END
- <div id="maincontent">
-   <div id="mainrow">
-    <div id="leftcol">
-	  <div id="bigtext">
-      <p>
-      Cosmology@Home lets you volunteer your spare computer time (like when your screen saver is on) 
-      to help search for the model which best describes our Universe and to find the range of models 
-      that agree with available cosmological and particle physics data. </p>
-    </div>
-	  <div id="majorlinks">
-    <!--[if lte IE 6]>
-    <div style="display: none">
-    <![endif]-->
-		 <div class="linkbtn"><a href="join.php"><img src="img/navigation/major_links/link_btn.png"/><span>> how to join</span></a></div>
-		 <div class="linkbtn"><a href="home.php"><img src="img/navigation/major_links/link_btn.png"/><span>> your account</span></a></div>
-		 <div class="linkbtn"><a href="forum_index.php"><img src="img/navigation/major_links/link_btn.png"/><span>> forums</span></a></div>
-		 <div class="linkbtn"><a href="downloads.php"><img src="img/navigation/major_links/link_btn.png"/><span>> goodies</span></a></div>
-    <!--[if lte IE 6]>
-    </div>
-    <![endif]-->
-	  </div>
-	  <div id="feature">
-	     <span class="h2"><img src="img/concept_icon.png" alt="concept" />FEATURED CONCEPT: Dark Matter</span>
-     <div class="featuretext">
-     Unlike ordinary matter, dark matter does not emit or absorb light--or any other type of electromagnetic radiation. Consequently, dark matter cannot be observed directly using a telescope or any other astronomical instrument that has been developed by humans. If dark matter has these strange properties, how do we know that it exists in the first place?
-     <br> 
-      Like ordinary matter, dark matter interacts gravitationally with ordinary matter and radiation. Astronomers study the distribution of dark matter through observing its gravitational effects on ordinary matter in its vicinity and through its gravitational lensing effects on background radiation. 
-      The background image shows the <i>bullet cluster</i>, a famous example where the visible matter does not follow the mass distribution.
-      <br>Combining all the available evidence, dark matter represents about 83% of the matter content of the universe.
-       Read more about dark matter on 
-     <a href=http://teacherlink.ed.usu.edu/tlnasa/reference/ImagineDVD/Files/imagine/docs/science/know_l1/dark_matter.html>this web page</a>. Let us know your questions and comments on the <a href=http://www.cosmologyathome.org/forum_thread.php?id=926>message board.</a></div>
-	  </div>
+    <div class="row">
+        <div class="col-sm-6" id="leftcol">
+            <div class="panel panel-default mb-3">
+                <div class="panel-body" id="bigtext">
+                Cosmology@Home lets you volunteer your spare computer time (like
+                when your screen saver is on) to help better understand our
+                universe by searching for the range of theoretical models that
+                agree with cutting-edge cosmological and particle physics data. 
+                </div>
+            </div>
+            <div class="panel panel-default mb-3" id="majorlinks">
+                <div class="panel-body">
+                    <div class="linkbtn"><a href="join.php"><img src="img/navigation/major_links/link_btn.png"/><span>> how to join</span></a></div>
+                    <div class="linkbtn"><a href="home.php"><img src="img/navigation/major_links/link_btn.png"/><span>> your account</span></a></div>
+                    <div class="linkbtn"><a href="forum_index.php"><img src="img/navigation/major_links/link_btn.png"/><span>> forums</span></a></div>
+                    <div class="linkbtn"><a href="downloads.php"><img src="img/navigation/major_links/link_btn.png"/><span>> goodies</span></a></div>
+                </div>
+            </div>
+            <div class="panel panel-default">
+                <div class='panel-heading'>FEATURED CONCEPT: Dark Matter</div>
+                <div class="panel-body featuretext" id="feature">
+                Unlike ordinary matter, dark matter does not emit or absorb
+                light--or any other type of electromagnetic radiation.
+                Consequently, dark matter cannot be observed directly using a
+                telescope or any other astronomical instrument that has been
+                developed by humans. If dark matter has these strange
+                properties, how do we know that it exists in the first place?
+                <br> 
+                Like ordinary matter, dark matter interacts gravitationally with
+                ordinary matter and radiation. Astronomers study the
+                distribution of dark matter through observing its gravitational
+                effects on ordinary matter in its vicinity and through its
+                gravitational lensing effects on background radiation.  The
+                background image shows the <i>bullet cluster</i>, a famous
+                example where the visible matter does not follow the mass
+                distribution. 
+                <br> 
+                Combining all the available evidence, dark
+                matter represents about 83% of the matter content of the
+                universe. Read more about dark matter on  <a
+                href=http://teacherlink.ed.usu.edu/tlnasa/reference/ImagineDVD/Files/imagine/docs/science/know_l1/dark_matter.html>this
+                web page</a>. Let us know your questions and comments on the <a
+                href=http://www.cosmologyathome.org/forum_thread.php?id=926>message
+                board.</a></div>
+            </div>
+        </div>
+        
+        <div class="col-sm-6" id="rightcol">
 END;
 
 if ($stopped) {
@@ -74,55 +90,44 @@ if ($stopped) {
     ";
 } else {
     db_init();
-    show_nav();
+    // show_nav();
 }
-
-echo <<<END
-       
-      </div><!--CLOSE LEFT COLUMN-->
-	  <!--<td id="rightcol">-->
-	 <div id="rightcol">
-END;
 
 if (!$stopped) {
     $profile = get_current_uotd();
     if ($profile) {
         echo "
-            <div id='uotd'>
-            <h2 class=headline>User of the Day</h2><br/><p>";
+            <div class='panel panel-default mb-3'>
+                <div class='panel-heading'>User of the Day</div>
+                <div class='panel-body'>";
         show_uotd($profile);
-        echo "</p></div>"; /*END USER OF THE DAY*/
+        echo "</div></div>"; /*END USER OF THE DAY*/
     }
 }
 
 echo "
-    <tr><td class=news>
-    <h2 class=headline>News</h2>
-      <p>
+            <div class='panel panel-default mb-3'>
+                <div class='panel-heading'>News</div>
+                <div class='panel-body'>
+                <tr><td class=news>
+                  <p>
 ";
 include("motd.php");
-
 show_news(0, 5);
 echo "
-    </td>
-    </tr></table>
+                </td>
+                </tr></table>
+            </div>
 ";
 
 echo <<<END
-
-	 </div>  <!--END RIGHT COLUMN-->
-  <!--</tr>-->
-  <!--</div>--> <!--END MAINROW-->
-  <!--</tbody>-->
-  <!--</table>-->
-  </div>
- </div> <!--END MAINCONTENT-->
- 
+        </div>
+    </div>
   
 END;
 
     include "schedulers.txt";
 
-    page_tail_main();
+    page_tail();
 
 ?>
